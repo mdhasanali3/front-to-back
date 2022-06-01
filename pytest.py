5a426ed3e3380765f576920fa90a30cbfca22171
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2022-06-01 13:56:32 +0600

    check path issue

---
+++ pytest.py
@@ -0,0 +1,12 @@
+# import requests
+import subprocess
+from app import test_meeting
+# r = requests.get("http://google.com")       
+# print(r.status_code)
+
+os_name = subprocess.run(["lsb_release", "-a"])
+print("The exit code was: %d" % os_name.returncode)
+
+def app():
+    print("hi success")
+app()
