387f69f3f0ed4d4e42174d6a70f965a6c668e31b
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2022-06-01 12:05:37 +0600

    Update pytest.py

--- schedule-meetings/backend_flask/pytest.py
+++ schedule-meetings/backend_flask/pytest.py
@@ -1,8 +1,8 @@
-import requests
+# import requests
 import subprocess
 
-r = requests.get("http://google.com")       
-print(r.status_code)
+# r = requests.get("http://google.com")       
+# print(r.status_code)
 
 os_name = subprocess.run(["lsb_release", "-a"])
 print("The exit code was: %d" % os_name.returncode)
